59447f0d7a870df54bf0d30f83f9fd362a1df09b
Amit Kumar <56325341+Amit-K-Dev@users.noreply.github.com>
2026-01-07 20:33:18 +0530

    Add basic list operations practice
    Add Python lists practice example

---
+++ lists.py
@@ -0,0 +1,16 @@
+# Python Lists Practice
+
+# Creating a list
+numbers = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print(numbers[0])   # first element
+print(numbers[-1])  # last element
+
+# Adding an element
+numbers.append(6)
+print(numbers)
+
+# Looping through a list
+for num in numbers:
+    print(num)
